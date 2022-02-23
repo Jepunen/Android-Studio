f6eb2ce24e1d528f6d4db31a1cbd6657547e6e62
Jepunen <62857023+Jepunen@users.noreply.github.com>
2022-02-23 17:47:11 +0200

    Update L7.java

--- L7.java
+++ L7.java
@@ -12,12 +12,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
